9feb2f5a82f7d101b07bb355ff5847113cce7385
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-02 10:28:26 +0200

    Add Props

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 
 class HeaderComponent extends React.Component{
   render(){  // єдиний метод, який ми зобов'язані визначити в класі
+    console.log(this.props);
     const p = React.createElement('p',{}, 'Super paragraph')
-    const h2 = React.createElement('h2',{title:'Hi!'},'Header')
+    const h2 = React.createElement('h2',{title:'Hi!'},`Hello ${this.props.name}`)
     const article = React.createElement('article',{},h2,p)
     return article;
   }
 }
-const componrnt = React.createElement(HeaderComponent)
+const componrnt = React.createElement(HeaderComponent,{name:'Nataliia'})
+const componrnt2 = React.createElement(HeaderComponent,{name:'Roland'})
+const parentElement = React.createElement('section',{},componrnt,componrnt2)
+
+
 const root = document.querySelector('#root')
 
-ReactDOM.render(componrnt, root)
+ReactDOM.render(parentElement, root)
