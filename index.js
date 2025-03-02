69ec13e2eb9ada6fc347237ded63006d7e0490f8
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-02 09:26:22 +0200

    Add classComponent

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 
+class HeaderComponent extends React.Component{
+  render(){  // єдиний метод, який ми зобов'язані визначити в класі
+    const p = React.createElement('p',{}, 'Super paragraph')
+    const h2 = React.createElement('h2',{title:'Hi!'},'Header')
+    const article = React.createElement('article',{},h2,p)
+    return article;
+  }
+}
+const componrnt = React.createElement(HeaderComponent)
 const root = document.querySelector('#root')
-const h1 = React.createElement('h1',{},'Hello React!') //реак елемент
-console.log(h1);
 
-const anotherH1 = document.createElement('h1') //HTML елемент
-console.dir(anotherH1);
-
-ReactDOM.render(h1, root)
-
-
-//-------------------------------------------------------------------
-
-/*
-Підключить react i react-dom
-Створіть елемент і вбудуйте його на сторінку
-Відрендоріть його і переконайтесь, що елемент відображається на сторінці
-
-*/
-const img = React.createElement('img', {
-    src:'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg='
-})
-
-ReactDOM.render(img, root)
+ReactDOM.render(componrnt, root)
