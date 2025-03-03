dd1d835532e36f563a9ee30faf2d956473c6c97b
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-03 11:24:33 +0200

    Add лічильник +,-

--- index.js
+++ index.js
@@ -1,32 +1,71 @@
 
+// class Counter extends React.Component{
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             count: 0
+//         };
+//     }
+//     increment(){
+//         // todo: state.count++
+//         this.setState({
+//             count: this.state.count +1
+//         })
+//         console.log('CLICK');
+//     }
+
+//     render(){
+//         const h2 = React.createElement('h2',{},this.state.count)
+//         const button = React.createElement('button',{onClick: ()=>{this.increment()}}, '+')
+//         return React.createElement(React.Fragment,{},h2,button);
+
+//     }
+// }
+// const component = React.createElement(Counter)
+
+// const root = document.querySelector('#root')
+// ReactDOM.render(component,root)
+//--------------------------------------------------------------------------
+
+/*
+Зробіть той самий counter, який крім інкременту буде ще й декрементувати лічильник
+Лічильник не має опускатися нижчі нуля
+
+*/
+
 class Counter extends React.Component{
     constructor(props){
         super(props)
-        this.state = {
+        this.state ={
             count: 0
-        };
+        }
     }
-    increment(){
-        // todo: state.count++
-        this.setState({
-            count: this.state.count +1
-        })
-        console.log('CLICK');
-    }
-
-    render(){
-        const h2 = React.createElement('h2',{},this.state.count)
-        const button = React.createElement('button',{onClick: ()=>{this.increment()}}, '+')
-        return React.createElement(React.Fragment,{},h2,button);
-
-    }
+increment(){
+    this.setState({
+        count: this.state.count + 1
+    })
 }
+
+decrement(){
+    this.setState({
+        count: this.state.count - 1
+    })
+}
+
+render(){
+    const h2 = React.createElement('h2', {},this.state.count)
+    const buttonIncrement = React.createElement('button', {onClick:()=>{this.increment()}},'+')
+    const buttonDecrement = React.createElement('button', {onClick:()=>{this.decrement()}},'-')
+    return React.createElement(React.Fragment,{}, h2,buttonIncrement,buttonDecrement)
+}
+
+}
+
 const component = React.createElement(Counter)
 
 const root = document.querySelector('#root')
+
 ReactDOM.render(component,root)
-
-
 
 
 
