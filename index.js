40e05ddcd6ba6bf8a60559415e8affef8dcd6521
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-03 11:51:02 +0200

    Add лічильник +,-не повинен бути менше 0

--- index.js
+++ index.js
@@ -1,31 +1,4 @@
 
-// class Counter extends React.Component{
-//     constructor(props){
-//         super(props)
-//         this.state = {
-//             count: 0
-//         };
-//     }
-//     increment(){
-//         // todo: state.count++
-//         this.setState({
-//             count: this.state.count +1
-//         })
-//         console.log('CLICK');
-//     }
-
-//     render(){
-//         const h2 = React.createElement('h2',{},this.state.count)
-//         const button = React.createElement('button',{onClick: ()=>{this.increment()}}, '+')
-//         return React.createElement(React.Fragment,{},h2,button);
-
-//     }
-// }
-// const component = React.createElement(Counter)
-
-// const root = document.querySelector('#root')
-// ReactDOM.render(component,root)
-//--------------------------------------------------------------------------
 
 /*
 Зробіть той самий counter, який крім інкременту буде ще й декрементувати лічильник
@@ -33,48 +6,64 @@
 
 */
 
-class Counter extends React.Component{
-    constructor(props){
-        super(props)
-        this.state ={
-            count: 0
-        }
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  increment() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
+  decrement() {
+    if (this.state.count > 0) {
+      this.setState({
+        count: this.state.count - 1,
+      })
+    } else if (this.state.count === 0){
+        alert('Лічильник не повинен дорівнювати 0')
     }
-increment(){
-    this.setState({
-        count: this.state.count + 1
-    })
+  }
+
+  render() {
+    const h2 = React.createElement("h2", {}, this.state.count);
+    const buttonIncrement = React.createElement(
+      "button",
+      {
+        onClick: () => {
+          this.increment();
+        },
+      },
+      "+"
+    );
+    const buttonDecrement = React.createElement(
+      "button",
+      {
+        onClick: () => {
+          this.decrement();
+        },
+      },
+      "-"
+    );
+    return React.createElement(
+      React.Fragment,
+      {},
+      h2,
+      buttonIncrement,
+      buttonDecrement
+    );
+  }
 }
 
-decrement(){
-    this.setState({
-        count: this.state.count - 1
-    })
-}
+const component = React.createElement(Counter);
 
-render(){
-    const h2 = React.createElement('h2', {},this.state.count)
-    const buttonIncrement = React.createElement('button', {onClick:()=>{this.increment()}},'+')
-    const buttonDecrement = React.createElement('button', {onClick:()=>{this.decrement()}},'-')
-    return React.createElement(React.Fragment,{}, h2,buttonIncrement,buttonDecrement)
-}
+const root = document.querySelector("#root");
 
-}
-
-const component = React.createElement(Counter)
-
-const root = document.querySelector('#root')
-
-ReactDOM.render(component,root)
-
-
-
-
-
-
-
-
-
+ReactDOM.render(component, root);
 
 //-------------------------------------------------------------------------------
 /*
@@ -95,4 +84,3 @@ ReactDOM.render(component,root)
 7. Стан-конпоненти - це такі данні, які можуть змінюватись за час життя нашої компоненти
 
 */
-
