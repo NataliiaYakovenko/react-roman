f7773f55ac48c5e3b299f226f230daed6dfc6643
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-05 12:54:42 +0200

    Add JSX

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,85 +1,54 @@
 import React from "react";
 
-
-
 class Counter extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        count: 0,
-        step: 1,
-      };
-    }
-    increment() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      step: 1,
+    };
+  }
+  increment() {
+    this.setState({
+      count: this.state.count + this.state.step,
+    });
+  }
+
+  decrement() {
+    if (this.state.count > 0) {
       this.setState({
-        count: this.state.count + this.state.step,
+        count: this.state.count - this.state.step,
       });
-    }
-  
-    decrement() {
-      if (this.state.count > 0) {
-        this.setState({
-          count: this.state.count - this.state.step,
-        });
-      } else if (this.state.count === 0 || (this.state.count - this.state.step) < 0) {
-        alert("Лічильник не повинен дорівнювати 0");
-      }
-    }
-  
-    render() {
-      const counter = React.createElement(
-        "h2",
-        {},
-        `Значення лічильника: ${this.state.count}`
-      );
-      const step = React.createElement(
-        "h2",
-        {},
-        `Значення кроку: ${this.state.step}`
-      );
-  
-      const buttonIncrement = React.createElement(
-        "button",
-        {
-          onClick: () => {
-            this.increment();
-          },
-        },
-        "+"
-      );
-  
-      const buttonDecrement = React.createElement(
-        "button",
-        {
-          onClick: () => {
-            this.decrement();
-          },
-        },
-        "-"
-      );
-  
-      const setStepButton = React.createElement(
-        "button",
-        {
-          onClick: () => {
-            this.setState({
-              step: Number(prompt("Введіть нове значення для кроку:"))
-            })
-          },
-        },
-        "Встановити нове значення для кроку"
-      );
-  
-      return React.createElement(
-        React.Fragment,
-        {},
-        counter,
-        buttonIncrement,
-        buttonDecrement,
-        step,
-        setStepButton
-      );
+    } else if (
+      this.state.count === 0 ||
+      this.state.count - this.state.step < 0
+    ) {
+      alert("Лічильник не повинен дорівнювати 0");
     }
   }
 
-  export default Counter; 
+  render() {
+    return(
+    <>
+      <h2>{this.state.count}</h2>
+      <h2>{this.state.step}</h2>
+      <button onClick={()=>{this.increment()}}>+</button>
+      <button onClick={()=>{this.decrement()}}>-</button>
+      <button onClick={()=>{this.setState({step: Number(prompt("Введіть нове значення для кроку:"))})}}>Встановити нове значення для кроку</button>
+
+    </>
+    )
+
+    //   return React.createElement(
+    //     React.Fragment,
+    //     {},
+    //     counter,
+    //     buttonIncrement,
+    //     buttonDecrement,
+    //     step,
+    //     setStepButton
+    //   );
+  }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Counter from "./components/Counter.js"
-import * as MyMath from "./components/Math.js"
-
-
-const component = React.createElement(Counter);
 
 const root = document.querySelector("#root");
 
-ReactDOM.render(component, root);
+ReactDOM.render(<Counter/>, root);
 
 MyMath.sum()
 
